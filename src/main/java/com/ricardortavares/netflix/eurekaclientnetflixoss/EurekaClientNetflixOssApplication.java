1cf5c78df949272f7423766123f5410868d9b648
Ricardo Rodrigues Tavares <ricardo.tavares@matera.com>
2019-02-26 14:26:39 -0300

    Commit Inicial

---
+++ src/main/java/com/ricardortavares/netflix/eurekaclientnetflixoss/EurekaClientNetflixOssApplication.java
@@ -0,0 +1,13 @@
+package com.ricardortavares.netflix.eurekaclientnetflixoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientNetflixOssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientNetflixOssApplication.class, args);
+	}
+
+}
